d3342e4c20b9b4344dbcaa5eb86098ffac93f9fc
Алексей Михайлов <alexeysampds@gmail.com>
2022-06-09 20:26:30 +0400

    Исправляем вспомогательные функции 2

--- js/main.js
+++ js/main.js
@@ -4,16 +4,16 @@ const getRandomNumber = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   if (min >= max) {
-    return('Ошибка! Минимальное число не может быть больше либо равно максимальному');
+    return 'Ошибка! Минимальное число не может быть больше либо равно максимальному';
   }
   if (min < 0 || max < 0) {
-    return('Можно использовать только диапазон положительных чисел, включая 0');
+    return 'Можно использовать только диапазон положительных чисел, включая 0';
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-(getRandomNumber(80, 40));
+getRandomNumber(80, 40);
 
 const checkingStringLength = (str, maxLength) => str.length <= maxLength;
 
-(checkingStringLength ('sdghfjsdf qweqw', 5));
+checkingStringLength ('sdghfjsdf qweqw', 5);
