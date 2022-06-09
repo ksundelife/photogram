ddd1754220516f1e4ededb8c9cdfe6d70d6d80f6
Алексей Михайлов <alexeysampds@gmail.com>
2022-06-09 06:19:26 +0400

    Исправляем вспомогательные функции

--- js/main.js
+++ js/main.js
@@ -1,27 +1,19 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-const getRandomNumber = function (min, max) {
+const getRandomNumber = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   if (min >= max) {
-    console.log('Ошибка! Минимальное число не может быть больше либо равно максимальному');
-    return;
+    return('Ошибка! Минимальное число не может быть больше либо равно максимальному');
   }
   if (min < 0 || max < 0) {
-    console.log('Можно использовать только диапазон положительных чисел, включая 0');
-    return;
+    return('Можно использовать только диапазон положительных чисел, включая 0');
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-console.log(getRandomNumber(0, 40));
+(getRandomNumber(80, 40));
 
-const checkLengthString = function (line, maxLength) {
-  if (line.length <= maxLength) {
-    return true;
-  } else {
-    return false;
-  }
-};
+const checkingStringLength = (str, maxLength) => str.length <= maxLength;
 
-console.log(checkLengthString ('sdghfjsdf', 3));
+(checkingStringLength ('sdghfjsdf qweqw', 5));
