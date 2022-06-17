431b48bba8bd6e403a6a94fea42a15212a91d2b0
Алексей Михайлов <alexeysampds@gmail.com>
2022-06-17 11:12:29 +0400

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,19 +1,72 @@
-// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const DESCRIPTION = [
+  'На природе',
+  'На море',
+  'На улице',
+  'На даче',
+  'Работа не волк, работа - это ворк, волк - это ходить',
+  'С друзьями',
+];
 
-const getRandomNumber = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  if (min >= max) {
-    return 'Ошибка! Минимальное число не может быть больше либо равно максимальному';
-  }
-  if (min < 0 || max < 0) {
-    return 'Можно использовать только диапазон положительных чисел, включая 0';
-  }
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+const MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон',
+];
+
+
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 };
 
-getRandomNumber(80, 40);
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
-const checkingStringLength = (str, maxLength) => str.length <= maxLength;
+const createPhotoDescription = () => {
+  const result = [];
 
-checkingStringLength ('sdghfjsdf qweqw', 5);
+  for (let i=1; i<=25; i++) {
+    const randomId = i;
+    const randomUrl = i;
+    const likes = getRandomPositiveInteger(15, 200);
+    const randomCommentId = getRandomPositiveInteger(1, Number.MAX_SAFE_INTEGER);
+    const randomAvatar = getRandomPositiveInteger(1, 6);
+
+    const comments = [];
+    const userInformation = {
+      id: randomCommentId,
+      avatar: `img/avatar-${randomAvatar}.svg`,
+      message: getRandomArrayElement(MESSAGE),
+      name: getRandomArrayElement(NAMES),
+    };
+    comments.push(userInformation);
+
+    result.push(
+      {
+        id: randomId,
+        url: `photos/${randomUrl}.jpg`,
+        description: getRandomArrayElement(DESCRIPTION),
+        likes: likes,
+        comments,
+      }
+    );
+  }
+
+  return result;
+};
+console.log(createPhotoDescription());
